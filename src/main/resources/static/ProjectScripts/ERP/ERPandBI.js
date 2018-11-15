edfee43f4d1616efe639cd5ed8d0d093286c36d4
1669333281 <1669333281@qq.com>
2018-11-15 17:51:21 +0800

    QZY

--- src/main/resources/static/ProjectScripts/ERP/ERPandBI.js
+++ src/main/resources/static/ProjectScripts/ERP/ERPandBI.js
@@ -544,7 +544,7 @@ function setYlly() {
                 symbolSize: 8,//拐点大小
                 /*yAxisIndex: 1,
                 xAxisIndex: 1,*/
-                data: [210, 470, 300, 480, 400, 590],
+                data: [210, 470, 300, 480, 400, 500],
                 itemStyle: {
                     normal: {
                         color: '#ee992f',
@@ -561,10 +561,10 @@ function setYlly() {
                             y2: 1,
                             colorStops: [{
                                 offset: 0,
-                                color: 'rgba(82,186,212,1)' // 0% 处的颜色
+                                color: 'rgba(255, 165,0,1)' // 0% 处的颜色
                             }, {
                                 offset: 1,
-                                color: 'rgba(82,186,212,0.2)' // 100% 处的颜色
+                                color: 'rgba(255, 165,0,0.2)' // 100% 处的颜色
                             }],
                             globalCoord: false // 缺省为 false
                         }
@@ -604,6 +604,15 @@ function setKc() {
 
     // 指定图表的配置项和数据
     var  option = {
+        title: {
+            text: '折线图堆叠',
+            x:'center',
+            textStyle: {
+                //文字颜色
+                color: '#208198',
+            },
+        },
+
         tooltip: {
             trigger: 'axis',
             axisPointer: {
@@ -615,7 +624,7 @@ function setKc() {
         },
         xAxis: [{
             type: 'category',
-            data: ['0601', '0602', '0603', '0604', '0605', '0606', '0607', '0608', '0609', '0610', '0611', '0612'],
+            data: ['汽油', '柴油', '液化气', '丙烷', '丙烯', '石油焦'],
             axisPointer: {
                 type: 'none'
             },
@@ -640,7 +649,7 @@ function setKc() {
                 type: 'category',
                 show: false,
                 position: '',
-                data: ['0601', '0602', '0603', '0604', '0605', '0606', '0607', '0608', '0609', '0610', '0611', '0612'],
+                data: ['汽油', '柴油', '液化气', '丙烷', '丙烯', '石油焦'],
                 offset:20,
                 axisPointer: {
                     type: 'none',
@@ -695,33 +704,9 @@ function setKc() {
 
             type: 'bar',
             //                barWidth:27,
-            data: [40, 50, 60, 30, 80, {
-                value: 70,
-                z: 3,
-                itemStyle: {
-                    normal: {
-                        barBorderRadius: 7,
-                        color: {
-                            type: 'linear',
-                            x: 0,
-                            y: 0,
-                            x2: 1,
-                            y2: 0,
-                            colorStops: [{
-                                offset: 0,
-                                color: '#f11dc8' // 0% 处的颜色
-                            }, {
-                                offset: 1,
-                                color: '#ff2575' // 100% 处的颜色
-                            }],
-                            globalCoord: false // 缺省为 false
-                        }
-                    },
-                    emphasis: {
-                        barBorderRadius: 7,
-                    }
-                },
-            }, 50, 24, 85, 66, 53, 35],
+            barWidth : 12,
+
+            data: [210, 470, 300, 480, 400, 500],
             itemStyle: {
                 normal: {
                     barBorderRadius: 7,
@@ -749,18 +734,20 @@ function setKc() {
         },
 
             {
-                name: '所占比例',
-                type: 'line',
-                yAxisIndex: 1,
-                xAxisIndex: 1,
-                symbol: 'none',
-                data: [160, 170, 140, 110, 160, 170, 130, 110, 125, 166, 143, 125],
+                /* name: '所占比例',*/
+                type:'line',
+                symbol:'star',//拐点样式
+                symbolSize: 8,//拐点大小
+                /*yAxisIndex: 1,
+                xAxisIndex: 1,*/
+                data: [210, 470, 300, 480, 400, 500],
                 itemStyle: {
                     normal: {
-                        color: '#51e5fc',
+                        color: '#06bbff',
                     }
                 },
                 areaStyle: {
+
                     normal: {
                         color: {
                             type: 'linear',
@@ -770,10 +757,10 @@ function setKc() {
                             y2: 1,
                             colorStops: [{
                                 offset: 0,
-                                color: 'rgba(82,186,212,1)' // 0% 处的颜色
+                                color: 'rgba(0,0,255,1)' // 0% 处的颜色
                             }, {
                                 offset: 1,
-                                color: 'rgba(82,186,212,0.2)' // 100% 处的颜色
+                                color: 'rgba(0,0,255,0.2)' // 100% 处的颜色
                             }],
                             globalCoord: false // 缺省为 false
                         }
@@ -800,7 +787,7 @@ function setKc() {
                         }
                     }
                 },
-                smooth: true
+                smooth: false
             }
         ]
     };
